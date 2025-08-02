6d42dc410b3effc89199cca13a3e637648f573c1
denys-chornopyskyi <chornopyskyidenys@gmail.com>
2025-08-02 20:47:04 +0200

    add contacts_to_text function

--- main.py
+++ main.py
@@ -15,3 +15,55 @@ def parse_contacts() -> list[dict]:
   return contacts
 
 contacts = parse_contacts()
+
+def contacts_to_string(contacts: list[dict]) -> None:
+  contacts = contacts.copy()
+  new_contacts = []
+  for contact in contacts:
+    line = f"{contact['name']}, {contact['phone']}, {contact['email']}\n"
+    new_contacts.append(line)
+  with open('contacts.txt', 'w') as file:
+    print(new_contacts)
+    file.writelines(new_contacts)
+
+def add_contact():
+  while True:
+    name = input('Введите имя: ')
+    if name.strip() == '':
+      continue
+    break
+
+  while True:
+    phone = input('Введите телефон: ')
+    if name.strip() == '':
+      continue
+    if len(phone) < 12:
+      continue
+    break
+
+  while True:
+    email = input('Введите email: ')
+    if name.strip() == '':
+      continue
+    if '@' not in email:
+      continue
+    break
+
+  print('✅ Контакт успешно добавлен!')
+
+      
+  contact = {
+      'name': name,
+      'phone': phone,
+      'email': email
+    }
+  contacts.append(contact)
+  
+  print(contacts)
+  
+  contacts_to_string(contacts)
+
+add_contact()
+
+
+
